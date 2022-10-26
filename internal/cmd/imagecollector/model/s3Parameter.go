b7293d80628df6ff368180695c38540deafcef7e
Timo Pagel <github@timo-pagel.de>
2022-10-26 18:43:59 +0200

    remove required for sa variables (#12)

--- internal/cmd/imagecollector/model/s3Parameter.go
+++ internal/cmd/imagecollector/model/s3Parameter.go
@@ -2,8 +2,8 @@ package model
 
 type S3parameterEntry struct {
 	S3bucket         string `validate:"required"`
-	S3secretKey      string `validate:"required"`
-	S3accessKey      string `validate:"required"`
+	S3secretKey      string
+	S3accessKey      string
 	S3endpoint       string `validate:"required"`
 	S3insecure       bool   `validate:"required"`
 	S3region         string `validate:"required"`
